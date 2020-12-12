84b4728d9e199598d38a3d4563d7496dfaae0b3d
Rodolfo Heredia <a2019115883@estudiantes.upsa.edu.bo>
2020-12-12 11:19:59 -0400

    Commit de operaciones

---
+++ FinalRodolfoHeredia/FinalRodolfoHeredia/Controllers/OperacionesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalRodolfoHeredia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperacionesController : ControllerBase
+    {
+        [HttpGet]
+        public string operaciones(int numero)
+        {
+            if (numero == 0)
+                return "Realizado por Rodolfo Heredia";
+            else if (numero < 0)
+                return "Error";
+            else
+                return "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg";
+
+        }
+    }
+}
